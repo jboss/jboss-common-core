d463f055e80f49d7f06654cc998d7a29b6290902
scott.stark@jboss.org <scott.stark@jboss.org@15291643-af16-0410-b526-a589213603f9>
2006-12-13 02:07:44 +0000

    Update javadoc
    git-svn-id: https://svn.jboss.org/repos/common/common-core/trunk@2217 15291643-af16-0410-b526-a589213603f9

--- src/main/java/org/jboss/util/graph/Edge.java
+++ src/main/java/org/jboss/util/graph/Edge.java
@@ -34,57 +34,89 @@ public class Edge<T>
    private int cost;
    private boolean mark;
 
-   // Create an edge with 0 cost
-   public Edge(Vertex<T> one, Vertex<T> two)
+   /**
+    * Create a zero cost edge between from and to
+    * @param from the starting vertex
+    * @param to the ending vertex 
+    */
+   public Edge(Vertex<T> from, Vertex<T> to)
    {
-      from = one;
-      to = two;
-      cost = 0;
+      this(from, to, 0);
+   }
+
+   /**
+    * Create an edge between from and to with the given cost.
+    * 
+    * @param from the starting vertex
+    * @param to the ending vertex
+    * @param cost the cost of the edge 
+    */
+   public Edge(Vertex<T> from, Vertex<T> to, int cost)
+   {
+      this.from = from;
+      this.to = to;
+      this.cost = cost;
       mark = false;
    }
 
-   // Create an edge and define the cost
-   public Edge(Vertex<T> one, Vertex<T> two, int c)
-   {
-      from = one;
-      to = two;
-      cost = c;
-      mark = false;
-   }
-
+   /**
+    * Get the ending vertex
+    * @return ending vertex
+    */
    public Vertex<T> getTo()
    {
       return to;
    }
 
+   /**
+    * Get the starting vertex
+    * @return starting vertex
+    */
    public Vertex<T> getFrom()
    {
       return from;
    }
 
+   /**
+    * Get the cost of the edge
+    * @return cost of the edge
+    */
    public int getCost()
    {
       return cost;
    }
 
-   // Mark an edge
+   /**
+    * Set the mark flag of the edge
+    *
+    */
    public void mark()
    {
       mark = true;
    }
 
-   // Clear the mark
+   /**
+    * Clear the edge mark flag
+    *
+    */
    public void clearMark()
    {
       mark = false;
    }
 
-   // Test the mark
+   /**
+    * Get the edge mark flag
+    * @return edge mark flag
+    */
    public boolean isMarked()
    {
       return mark;
    }
 
+   /**
+    * String rep of edge
+    * @return string rep with from/to vertex names and cost
+    */
    public String toString()
    {
       StringBuffer tmp = new StringBuffer("Edge[from: ");
