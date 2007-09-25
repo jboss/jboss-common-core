d3623c92b03194a5653fdc105b01b2e9bbbe6a18
dimitris@jboss.org <dimitris@jboss.org@15291643-af16-0410-b526-a589213603f9>
2007-09-25 15:30:34 +0000

    JBCOMMON-24, handle null values and multiple invocations of hasNext()
    git-svn-id: https://svn.jboss.org/repos/common/common-core/trunk@2583 15291643-af16-0410-b526-a589213603f9

--- src/main/java/org/jboss/util/collection/WeakSet.java
+++ src/main/java/org/jboss/util/collection/WeakSet.java
@@ -1,33 +1,32 @@
 /*
-  * JBoss, Home of Professional Open Source
-  * Copyright 2005, JBoss Inc., and individual contributors as indicated
-  * by the @authors tag. See the copyright.txt in the distribution for a
-  * full listing of individual contributors.
-  *
-  * This is free software; you can redistribute it and/or modify it
-  * under the terms of the GNU Lesser General Public License as
-  * published by the Free Software Foundation; either version 2.1 of
-  * the License, or (at your option) any later version.
-  *
-  * This software is distributed in the hope that it will be useful,
-  * but WITHOUT ANY WARRANTY; without even the implied warranty of
-  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
-  * Lesser General Public License for more details.
-  *
-  * You should have received a copy of the GNU Lesser General Public
-  * License along with this software; if not, write to the Free
-  * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
-  * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
-  */
+ * JBoss, Home of Professional Open Source
+ * Copyright 2006, JBoss Inc., and individual contributors as indicated
+ * by the @authors tag. See the copyright.txt in the distribution for a
+ * full listing of individual contributors.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
 package org.jboss.util.collection;
 
 import java.lang.ref.ReferenceQueue;
-
-import java.util.Set;
-import java.util.HashSet;
 import java.util.AbstractSet;
+import java.util.HashSet;
 import java.util.Iterator;
 import java.util.NoSuchElementException;
+import java.util.Set;
 
 import org.jboss.util.NullArgumentException;
 import org.jboss.util.WeakObject;
@@ -117,10 +116,16 @@ public class WeakSet
             /** The set's iterator */
             Iterator iter = set.iterator();
 
+            /** JBCOMMON-24, handle null values and multiple invocations of hasNext() */
+            Object UNKNOWN = new Object();
+            
             /** The next available object. */
-            Object next = null;
+            Object next = UNKNOWN;
 
             public boolean hasNext() {
+               if (next != UNKNOWN) {
+                  return true;
+               }
                while (iter.hasNext()) {
                   WeakObject weak = (WeakObject)iter.next();
                   Object obj = null;
@@ -128,21 +133,18 @@ public class WeakSet
                      // object has been reclaimed by the GC
                      continue;
                   }
-
                   next = obj;
                   return true;
                }
-
                return false;
             }
 
             public Object next() {
-               if ((next == null) && !hasNext()) {
+               if ((next == UNKNOWN) && !hasNext()) {
                   throw new NoSuchElementException();
                }
-
                Object obj = next;
-               next = null;
+               next = UNKNOWN;
 
                return obj;
             }
---
+++ src/test/java/org/jboss/test/util/test/collection/WeakSetUnitTestCase.java
@@ -0,0 +1,41 @@
+package org.jboss.test.util.test.collection;
+
+import java.util.Iterator;
+
+import org.jboss.util.collection.WeakSet;
+
+import junit.framework.TestCase;
+
+/**
+ * Unit tests for WeakSet
+ * 
+ * @author <a href="mailto:sven@meiers.net">Sven Meier</a> 
+ * @version $Revision: 43534 $
+ */
+public class WeakSetUnitTestCase extends TestCase
+{
+   public void testNullElement()
+   {
+      WeakSet set = new WeakSet();
+        
+      set.add(null);
+        
+      assertEquals(1, set.size());
+        
+      Iterator iterator = set.iterator();
+      assertTrue(iterator.hasNext());     
+      iterator.next();
+      assertFalse(iterator.hasNext());
+   }
+    
+   public void testMultipleHasNext()
+   {
+      WeakSet set = new WeakSet();
+        
+      set.add(new Object());
+        
+      Iterator iterator = set.iterator();
+      assertTrue(iterator.hasNext());
+      assertTrue(iterator.hasNext());
+   }
+}
