c39795d954b526d5009435af901520067c665771
adrian@jboss.org <adrian@jboss.org@15291643-af16-0410-b526-a589213603f9>
2008-03-31 18:17:26 +0000

    Add a test for scheduling work when the thread is interrupted
    git-svn-id: https://svn.jboss.org/repos/common/common-core/trunk@2788 15291643-af16-0410-b526-a589213603f9

---
+++ src/test/java/org/jboss/test/util/test/concurrent/ThreadPoolInterruptedThreadUnitTestCase.java
@@ -0,0 +1,78 @@
+/*
+ * JBoss, Home of Professional Open Source
+ * Copyright 2007, Red Hat Middleware LLC, and individual contributors
+ * by the @authors tag. See the copyright.txt in the distribution for a
+ * full listing of individual contributors.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+package org.jboss.test.util.test.concurrent;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicBoolean;
+
+import org.jboss.util.threadpool.BasicTaskWrapper;
+import org.jboss.util.threadpool.BasicThreadPool;
+import org.jboss.util.threadpool.RunnableTaskWrapper;
+import org.jboss.util.threadpool.Task;
+import org.jboss.logging.Logger;
+import junit.framework.TestCase;
+
+/**
+ * Tests of thread pool with Tasks added to the pool
+ *
+ * @see org.jboss.util.threadpool.ThreadPool
+ * @author <a href="adrian@jboss.org">Adrian.Brock</a>
+ * @author Scott.Stark@jboss.org
+ * @version $Revision: 2787 $
+ */
+public class ThreadPoolInterruptedThreadUnitTestCase extends TestCase
+{
+   public ThreadPoolInterruptedThreadUnitTestCase(String name)
+   {
+      super(name);
+   }
+
+   public static class TestRunnable implements Runnable
+   {
+      public CountDownLatch latch = new CountDownLatch(1);
+      public void run()
+      {
+         latch.countDown();
+      }
+   }
+   
+   public void testInterruptedExecute() throws Exception
+   {
+      BasicThreadPool pool = new BasicThreadPool();
+      TestRunnable runnable = new TestRunnable();
+      
+      Thread.currentThread().interrupt();
+      try
+      {
+         pool.run(runnable);
+      }
+      finally
+      {
+         assertTrue(Thread.interrupted());
+      }
+      assertTrue(runnable.latch.await(10, TimeUnit.SECONDS));
+   }
+}
